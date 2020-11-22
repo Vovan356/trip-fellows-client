5575a8536354e5ca3f6caa3d6f7cbcefa1e62249
Vovan356 <vova19907@gmail.com>
2020-11-23 00:41:50 +0300

    Test 1

--- authorization/src/main/java/com/tripfellows/authorization/MainActivity.kt
+++ authorization/src/main/java/com/tripfellows/authorization/MainActivity.kt
@@ -14,6 +14,7 @@ import com.tripfellows.authorization.util.MenuItemEnum
 
 
 
+
 class MainActivity : AppCompatActivity(), MainRouter {
     private lateinit var bottomNavigationView: BottomNavigationView
 
